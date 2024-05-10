35e4c9ee129ead3082bda8668044283d7fe3b9e4
youngbloood <944168348@qq.com>
2024-05-10 11:36:06 +0800

    chore: log

--- core/src/tsuixuq.rs
+++ core/src/tsuixuq.rs
@@ -2,47 +2,144 @@ use crate::message::Message;
 use crate::{channel::Channel, topic::Topic};
 use anyhow::anyhow;
 use anyhow::Result;
+use clap::Parser;
+use config::{Config, File};
 use std::collections::HashMap;
 use std::sync::Arc;
 use tokio::sync::mpsc::{self, Receiver, Sender};
+use tracing::Level;
+use tracing_appender::rolling::{daily, hourly, minutely, never, RollingFileAppender};
 
 const DEFAULT_BUFFER: u64 = 10000;
 const DEFAULT_FACTOR: u16 = 100;
 const DEFAULT_NUM: u64 = 10000;
 
-#[derive(Clone, Copy)]
+#[derive(Parser, Debug, serde::Deserialize)]
 pub struct TsuixuqOption {
-    pub tcp_port: u16,               // tcp监听端口
-    pub params_num_buffer: u16,      // 客户端发送的params缓存数量
-    pub memory_message_size: u64,    // 内存中存储message的大小
-    pub memory_message_count: u64,   // 内存中存储message的数量
-    pub message_flush_factor: u16,   // messag flush至disk因子
-    pub message_flush_interval: u64, // messaage flush至disk的时间间隔
-    pub topic_message_buffer: u64,   // topic中message数量缓存数
-    pub channel_message_buffer: u64, // channel中的message数量缓存数
-    pub channel_num_in_topic: u64,   // 一个topic下的channel数量
-    pub topic_num_in_tsuixuq: u64,   // 一个tsuixuq下的topic数量
+    #[arg(long = "log-dir", default_value = "")]
+    pub log_dir: String,
 
-    pub client_timeout: u16,       // 一个client多长时间没收到包，单位(s)
+    #[arg(long = "log-filename", default_value = "")]
+    pub log_filename: String,
+
+    #[arg(long = "log-level", default_value = "debug")]
+    pub log_level: String,
+
+    #[arg(long = "log-rolling", default_value = "")]
+    pub log_rolling: String,
+
+    #[arg(short = 'p', long = "port", default_value_t = 3890)]
+    pub tcp_port: u16, // tcp监听端口
+
+    #[arg(long = "msg-num-buffer", default_value_t = DEFAULT_BUFFER as u16)]
+    pub msg_num_buffer: u16, // 客户端发送的params缓存数量
+
+    #[arg(long = "memory-msg-size", default_value_t = DEFAULT_BUFFER)]
+    pub memory_message_size: u64, // 内存中存储message的大小
+
+    #[arg(long = "memory-msg-count", default_value_t = DEFAULT_BUFFER)]
+    pub memory_message_count: u64, // 内存中存储message的数量
+
+    #[arg(long = "msg-flush-factor", default_value_t = DEFAULT_FACTOR)]
+    pub message_flush_factor: u16, // messag flush至disk因子
+
+    #[arg(long = "msg-flush-interval", default_value_t = DEFAULT_FACTOR as u64)]
+    pub message_flush_interval: u64, // messaage flush至disk的时间间隔
+
+    #[arg(long = "topic-msg-buffer", default_value_t = DEFAULT_BUFFER)]
+    pub topic_message_buffer: u64, // topic中message数量缓存数
+
+    #[arg(long = "channel-msg-buffer", default_value_t = DEFAULT_BUFFER)]
+    pub channel_message_buffer: u64, // channel中的message数量缓存数
+
+    #[arg(long = "channel-num-in-topic", default_value_t = DEFAULT_BUFFER)]
+    pub channel_num_in_topic: u64, // 一个topic下的channel数量
+
+    #[arg(long = "topic-num-in-tsuixuq", default_value_t = DEFAULT_BUFFER)]
+    pub topic_num_in_tsuixuq: u64, // 一个tsuixuq下的topic数量
+
+    #[arg(long = "client-timeout", default_value_t = 30)]
+    pub client_timeout: u16, // 一个client多长时间没收到包，单位(s)
+
+    #[arg(long = "client-timeout-count", default_value_t = 3)]
     pub client_timeout_count: u16, // 一个client超时次数限制，超过该限制client会主动断开
 }
 
 impl TsuixuqOption {
-    pub fn new() -> Self {
-        TsuixuqOption {
-            params_num_buffer: DEFAULT_BUFFER as u16,
-            tcp_port: 3890,
-            memory_message_size: DEFAULT_BUFFER,
-            memory_message_count: DEFAULT_BUFFER,
-            message_flush_factor: DEFAULT_FACTOR,
-            message_flush_interval: DEFAULT_BUFFER,
-            topic_message_buffer: DEFAULT_BUFFER,
-            channel_message_buffer: DEFAULT_BUFFER,
-            channel_num_in_topic: DEFAULT_NUM,
-            topic_num_in_tsuixuq: DEFAULT_NUM,
-            client_timeout: 30,
-            client_timeout_count: 3,
+    // pub fn new() -> Self {
+    //     TsuixuqOption {
+    //         msg_num_buffer: DEFAULT_BUFFER as u16,
+    //         tcp_port: 3890,
+    //         memory_message_size: DEFAULT_BUFFER,
+    //         memory_message_count: DEFAULT_BUFFER,
+    //         message_flush_factor: DEFAULT_FACTOR,
+    //         message_flush_interval: DEFAULT_BUFFER,
+    //         topic_message_buffer: DEFAULT_BUFFER,
+    //         channel_message_buffer: DEFAULT_BUFFER,
+    //         channel_num_in_topic: DEFAULT_NUM,
+    //         topic_num_in_tsuixuq: DEFAULT_NUM,
+    //         client_timeout: 30,
+    //         client_timeout_count: 3,
+    //     }
+    // }
+
+    pub fn from_config(filename: &str) -> Result<TsuixuqOption> {
+        let mut opt = TsuixuqOption::parse();
+        if filename.len() != 0 {
+            let cfg = Config::builder()
+                .add_source(File::with_name(filename))
+                .build()?;
+            opt = cfg.try_deserialize()?;
         }
+
+        Ok(opt)
+    }
+
+    pub fn get_log_level(&self) -> Level {
+        match self.log_level.as_str() {
+            "info" => Level::INFO,
+            "warn" => Level::WARN,
+            "error" => Level::ERROR,
+            "debug" => Level::DEBUG,
+            "trace" => Level::TRACE,
+            _ => Level::INFO,
+        }
+    }
+
+    pub fn init_log(&self) -> Result<()> {
+        // 初始化并设置日志格式
+        let suber = tracing_subscriber::fmt()
+            .with_max_level(self.get_log_level())
+            .with_ansi(false);
+
+        if self.log_dir.len() == 0 && self.log_filename.len() == 0 {
+            suber.init();
+            return Ok(());
+        }
+
+        let appender: RollingFileAppender;
+        match self.log_rolling.as_str() {
+            "hourly" => {
+                appender = hourly(self.log_dir.as_str(), self.log_filename.as_str());
+            }
+
+            "minutely" => {
+                appender = minutely(self.log_dir.as_str(), self.log_filename.as_str());
+            }
+
+            "never" => {
+                appender = never(self.log_dir.as_str(), self.log_filename.as_str());
+            }
+
+            _ => {
+                appender = daily(self.log_dir.as_str(), self.log_filename.as_str());
+            }
+        }
+
+        let (non_blocking, _guard) = tracing_appender::non_blocking(appender);
+        suber.with_writer(non_blocking).init();
+
+        Ok(())
     }
 }
 
--- tsuixuqd/src/main.rs
+++ tsuixuqd/src/main.rs
@@ -1,30 +1,35 @@
 use anyhow::{anyhow, Result};
+use clap::Parser;
 use common::global;
 use core::{tsuixuq::TsuixuqOption, tsuixuqd::Tsuixuqd};
 use tokio::{select, signal};
-use tracing::Level;
+
+#[derive(Parser, Debug)]
+pub struct Config {
+    #[arg(short = 'c', long = "config", default_value = "")]
+    filename: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let subcriber = tracing_subscriber::fmt()
-        .with_max_level(Level::TRACE)
-        .finish();
-    tracing::subscriber::set_global_default(subcriber)?;
+    // 解析是否有配置文件
+    let cfg = Config::parse();
+    let opt = TsuixuqOption::from_config(cfg.filename.as_str())?;
+    // 初始化日志subcriber
+    opt.init_log()?;
 
-    let opt = TsuixuqOption::new();
     let mut daemon = Tsuixuqd::new(opt);
-
     select! {
-      result =  daemon.serve() => {
-         if let Err(err) = result{
-            return Err(anyhow!(err));
-         }
-       }
+        result =  daemon.serve() => {
+            if let Err(err) = result{
+                return Err(anyhow!(err));
+            }
+        }
 
-      sig = signal::ctrl_c() => {
-         eprintln!("recieve signal: {:?}", sig);
-         global::cancel()
-      }
+        sig = signal::ctrl_c() => {
+            eprintln!("recieve signal: {:?}", sig);
+            global::cancel()
+        }
     }
 
     Ok(())
